86eaef7db02e4cd27bc88b91ee6a1f4823705b96
Tau Teng Chong <ttc14@potoo01.doc.ic.ac.uk>
2017-12-12 00:58:28 +0000

    Changed Main.Cpp

--- ChessMain.cpp
+++ ChessMain.cpp
@@ -9,90 +9,76 @@ using namespace std;
 
 int main() {
 
-    cout << "===========================" << endl;
-    cout << "Testing the Chess Engine" << endl;
-    cout << "===========================" << endl;
-    cout << endl;
+  cout << "===========================" << endl;
+  cout << "Testing the Chess Engine" << endl;
+  cout << "===========================" << endl;
+  cout << endl;
 
-    ChessBoard cb;
+  ChessBoard cb;
+  cout << endl;
 
-    cout << endl;
+  cb.submitMove("D7", "D6");
+  cout << endl;
 
-    cb.submitMove("C2", "C4");
-    cout << endl;
+  cb.submitMove("D4", "H6");
+  cout << endl;
 
-    cb.submitMove("D7", "D5");
-    cout << endl;
-    
-    cb.submitMove("D1", "A4");
-    cout << endl;
-    
-    cb.submitMove("E7", "E5");
-    cout << endl;
+  cb.submitMove("D2", "D4");
+  cout << endl;
 
-    cb.submitMove("F2", "F4");
-    cout << endl;
-    
-    cb.submitMove("D8", "H4");
-    cout << endl;
+  cb.submitMove("F8", "B4");
+  cout << endl;
 
-    // cb.submitMove("C3", "C4");
-    // cout << endl;
+  cout << "===========================" << endl;
+  cout << "Alekhine vs. Vasic (1931)" << endl;
+  cout << "===========================" << endl;
+  cout << endl;
 
-    // cb.submitMove("C6", "C5");
-    // cout << endl;
+  cb.resetBoard();
+  cout << endl;
 
-      
-    // cout << "===========================" << endl;
-    // cout << "Alekhine vs. Vasic (1931)" << endl;
-    // cout << "===========================" << endl;
-    // cout << endl;
-    
-    // cb.resetBoard();
-    // cout << endl;
-    
-    // cb.submitMove("E2", "E4");
-    // cb.submitMove("E7", "E5");
-    // cout << endl;
-    
-    // cb.submitMove("E1", "E2");
-    // cb.submitMove("E8", "E7");
-    // cout << endl;
-    /*
-    cb.submitMove("B1", "C3");
-    cb.submitMove("F8", "B4");
-    cout << endl;
+  cb.submitMove("E2", "E4");
+  cb.submitMove("E7", "E6");
+  cout << endl;
 
-    cb.submitMove("F1", "D3");
-    cb.submitMove("B4", "C3");
-    cout << endl;
+  cb.submitMove("D2", "D4");
+  cb.submitMove("D7", "D5");
+  cout << endl;
 
-    cb.submitMove("B2", "C3");
-    cb.submitMove("H7", "H6");
-    cout << endl;
+  cb.submitMove("B1", "C3");
+  cb.submitMove("F8", "B4");
+  cout << endl;
 
-    cb.submitMove("C1", "A3");
-    cb.submitMove("B8", "D7");
-    cout << endl;
+  cb.submitMove("F1", "D3");
+  cb.submitMove("B4", "C3");
+  cout << endl;
 
-    cb.submitMove("D1", "E2");
-    cb.submitMove("D5", "E4");
-    cout << endl;
+  cb.submitMove("B2", "C3");
+  cb.submitMove("H7", "H6");
+  cout << endl;
 
-    cb.submitMove("D3", "E4");
-    cb.submitMove("G8", "F6");
-    cout << endl;
+  cb.submitMove("C1", "A3");
+  cb.submitMove("B8", "D7");
+  cout << endl;
 
-    cb.submitMove("E4", "D3");
-    cb.submitMove("B7", "B6");
-    cout << endl;
+  cb.submitMove("D1", "E2");
+  cb.submitMove("D5", "E4");
+  cout << endl;
 
-    cb.submitMove("E2", "E6");
-    cb.submitMove("F7", "E6");
-    cout << endl;
+  cb.submitMove("D3", "E4");
+  cb.submitMove("G8", "F6");
+  cout << endl;
 
-    cb.submitMove("D3", "G6");
-    cout << endl;
-    */
-    return 0;
+  cb.submitMove("E4", "D3");
+  cb.submitMove("B7", "B6");
+  cout << endl;
+
+  cb.submitMove("E2", "E6");
+  cb.submitMove("F7", "E6");
+  cout << endl;
+
+  cb.submitMove("D3", "G6");
+  cout << endl;
+
+  return 0;
 }
